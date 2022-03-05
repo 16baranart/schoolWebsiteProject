89529b56166f9fd1ca0f16b14e8fbf6641e96523
16baranart <16baranart@datrfordgrammarschool.org.uk>
2022-03-05 22:16:45 +0000

    finished with a proof of concept, might requite significant changes if i were to implement my next major goal, allowing multiple websites, and a way to input posts, but, overall, this is a workign demo:)

--- backEnd.py
+++ backEnd.py
@@ -1,45 +1,43 @@
-# the imports so far, LEAVE THEM ALONE, if you wanna do anything, add to them in a seperate list 
+# the imports so far, LEAVE THEM ALONE, if you wanna do anything, add to them in a seperate list
 import pymongo
 import datetime as dt
 
-# space for your imports:
-
-
-
-
-
+# space for your imports, expand as neccessary
+# import whatever.py
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 # this is important and allows for the database to work, dont touch it
-myClient = pymongo.MongoClient("mongodb://localhost:27017/")
-myDB = myClient["mydatabase"]
-mCollec = myDB["posts"]
+def makeDB():
+    Client = pymongo.MongoClient("mongodb://localhost:27017/")
+    DataBase = Client["mydatabase"]
+    collection = DataBase["posts"]
+    
+    return collection
 
 
-# this is largely unimportant, its what im trying to construct the posts out of, you can add to it but please dont remove anything
+# this is largely unimportant, its what im trying to construct the posts out of, you can add to it but dont remove
 dic = {"postType" : "", 
               "title" : "", 
               "text" : "", 
               "date+time" : ""
     }
 
-
-# prints out all the items in the collection
+# prints out all the items in the collection ----- this might be useless, remove if such
 def displayInCollection():
     for x in mCollec.find():
-        print(x)
+        print(x["title"])
 
-# this is a command line interpretation of what i want from the final product, leave it alone pls
+# this is a command line interpretation of what i want from the final product, getting the data from the website itself
 def makePost():
-    time = dt.datetime.now()
+    time = dt.datetime.now() # gets the time so that i can be stored
     print("""What is post type: 
           a) announcement
           b) news
           c) work
           """)
     inp = input()
-    if (inp=="a"):
+    if (inp=="a"): # this will be something that decides how it should be outputted later on
         postType = "announcement"
     elif inp=="b":
         postType = "news"
@@ -57,11 +55,34 @@ def makePost():
               "date+time" : time}
     addDicToCollection(mCollec, dic)
 
+# this makes a post into it's useful vars from an inputed dictionary
+def storePostAsVars(dictionary):
+    title = dictionary["title"]
+    text = dictionary["text"]
+    type = dictionary["postType"]
+    
+    return title, text, type
+
 # this adds a dictionary to the collection
 def addDicToCollection(collection, dictionary):
     collection.insert_one(dictionary)
 
-
-def formatPosts():
-    pass
+# sorts a collection by an input
+def sortCollection(collection, sort, ascending):
     
+    if ascending:
+        return collection.find().sort(sort, 1)
+    else:
+        return collection.find().sort(sort, -1)
+
+if __name__ == "__main__": # Will only run if the program has
+    
+    mCollec = makeDB()
+    
+    dicList = sortCollection(mCollec, "date+time", False)
+    for x in dicList:
+        title, text, _ = storePostAsVars(x)
+        print(title)
+    
+
+
--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import backEnd as BE
+import flask
 
+app = flask.Flask(__name__)
 
+def makePostWebsite():
+    webPre = """<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Posts</title>
 
-# this is like the main loop in java, use when running, otherwise leave it alone
-if __name__ == "__main__":
+        </head>
+
+        <body>"""
     
-    BE.makePost()
+    webMain = ""
+    webSuff = """
     
+        </body>
+    </html>
+    """
+    
+    dicList = BE.sortCollection(BE.makeDB(), "date+time", False)
+    for x in dicList:
+        title, text, _ = BE.storePostAsVars(x)
+        
+        webMain += f"<h1>{title}</h1><p>{text}"
+    
+    webSite = webPre +  " " + webMain + " " +  webSuff
+    
+    return webSite
+
+
+homeS = makePostWebsite()
+
+# HOME PAGE ------------------------------------------------------------------------------------------- #
+@app.route("/") # If you type in website.com/, it redirects you to website.com/home
+@app.route("/home") # The following function is what happens when you go to website.com/home
+def home():
+    return homeS # Passes the "posts" variable to the homepage, so they can be installed
+
+
+# RUN ------------------------------------------------------------------------------------------------- #
+if __name__ == "__main__": # Will only run if the program has
+    #BE.displayInCollection()
+    app.run(debug=True) # Debug=True allows the program's changes to take effect during runtime

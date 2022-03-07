f2bb246fbf6eb33d7e1145daa71e59b55b3b7911
16baranart <16baranart@datrfordgrammarschool.org.uk>
2022-03-07 18:42:58 +0000

    finally got the favicon stuff working :)

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import backEnd as BE
 import flask
+import os
 
 app = flask.Flask(__name__)
-titles = []
-texts = []
-
-
-
+app.config['SECRET_KEY'] = os.urandom(32)
 
 
 # HOME PAGE ------------------------------------------------------------------------------------------- #
 @app.route("/") # If you type in website.com/, it redirects you to website.com/home
 @app.route("/home") # The following function is what happens when you go to website.com/home
 def home():
+    flask.flash("hi")
     return flask.render_template("home.html") # Passes the "posts" variable to the homepage, so they can be installed
 
 
